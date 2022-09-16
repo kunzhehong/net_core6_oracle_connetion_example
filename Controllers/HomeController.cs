152f99cc53ffd05933b348cabf4bc48ea4751471
kunzhehong <kunzhehong@gmail.com>
2022-09-16 17:26:43 +0800

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using app.Models;
+using Oracle.ManagedDataAccess.Client;
+
+namespace app.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [Route("[controller]/test")]
+    public IActionResult test()
+    {
+        string connetString = "User Id=system;Password=oracle;Data Source=localhost:1521/xe";
+        string x = string.Empty;
+        using (OracleConnection con = new OracleConnection(connetString))
+        {
+            using (OracleCommand cmd = con.CreateCommand())
+            {
+                con.Open();
+                cmd.BindByName = true;
+                cmd.CommandText = "SELECT txt FROM demo";
+                
+                OracleDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    x = reader.GetString(0);
+                }
+
+            }
+        }
+            return this.Ok(new { name = "ZHE", age = 34, data = x });
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
